3faca3f06565f679f516f4076f833277ff4ed8f9
Abito Prakash <abito.prakash@nutanix.com>
2024-03-28 13:38:07 +0530

    Fix typo in console log message

--- src/components/random2.js
+++ src/components/random2.js
@@ -10,7 +10,7 @@ const ExampleComponent = () => {
   return (
     <div
       onClick={() =>
-        console.log('Cliscssked without properssssss role or key event handlers')
+        console.log('Cliscssked without prosperssssss role or key event handlers')
       }
     >
       Click me!

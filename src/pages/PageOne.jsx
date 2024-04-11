3be4e937243d40b1de7c1a92113761a9b77e1feb
Abito Prakash <abito.prakash@nutanix.com>
2024-04-11 16:31:00 +0530

    Add PageOne component to src/pages/PageOne.jsx

---
+++ src/pages/PageOne.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageOne = () => {
+  return (
+    <div>
+      <h1 onClick={() => console.log('page 1')}>Page One</h1>
+    </div>
+  );
+}
+
+export default PageOne;

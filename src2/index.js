fe09ad93cc37c9fbc8febad23f252be5acb8a4fd
Abito Prakash <abito.prakash@nutanix.com>
2025-07-17 00:42:01 +0530

    Test changes

--- src2/index.js
+++ src2/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <div>Hello, React!!!!</div>;
+const App = () => <div>Hello, React!</div>;
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)

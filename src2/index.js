12a2883ef0e5ac60f7f784da696860f13d06473d
Abito Prakash <abito.prakash@nutanix.com>
2025-07-17 14:17:56 +0530

    Add comments to index.js for better clarity on app structure (#44)

--- src2/index.js
+++ src2/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Amazing React App
+// This is a simple React application that renders a greeting message.
+// It demonstrates the basic structure of a React app with a functional component.
 const App = () => <div>Hello, React!!!!</div>;
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)

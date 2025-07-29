27dee1dc20882e35914104c02c50837bb94f0df8
Abito Prakash <abito.prakash@nutanix.com>
2025-07-29 13:26:21 +0530

    Improving workflows

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-// Examplsse component to test jsx-a11y and unicorn rules
-const App = () => <div>Hello, React!</div>;
+const App = () => <div>Hello, React! </div>;
 
 // Hello World!
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)

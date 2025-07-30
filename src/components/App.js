7e9db4d844b47a06d7f7c96bdce52607692e3182
Abito Prakash <abito.prakash@nutanix.com>
2025-07-30 09:22:08 +0530

    Random

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ const ExampleComponent = () => {
     <div
       role="button"
       onClick={() =>
-        console.log('sa without propersssss role or key event handlers')
+        console.log('sa without proper role or key event handlers')
       }
     >
       Click me!
